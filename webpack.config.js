b78d7a939c23b4f4898460cf42587ac69899ea36
Gogyungho <whohow10@gmail.com>
2023-01-27 17:03:20 +0900

    clean webpack plugin 셋팅

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const childProcess = require("child_process");
 const HtmlWebapckPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -50,5 +51,6 @@ module.exports = {
             }
           : false,
     }),
+    new CleanWebpackPlugin({}),
   ],
 };
