61d90a641271219cee7b64e62a74b7bd8fcdcc37
Gogyungho <whohow10@gmail.com>
2023-01-28 00:02:18 +0900

    바벨 플러그인 세팅및 테스트

---
+++ my-babel-plugin.js
@@ -0,0 +1,13 @@
+module.exports = function myBabelplugin() {
+  return {
+    visitor: {
+      VariableDeclaration(path) {
+        console.log("VariableDeclaration() kind:", path.node.kind); // const
+
+        if (path.node.kind === "const") {
+          path.node.kind = "var";
+        }
+      },
+    },
+  };
+};
