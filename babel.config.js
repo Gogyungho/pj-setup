da3bb7756943f1e70a023f6df8d3e15eb2b2112d
Gogyungho <whohow10@gmail.com>
2023-01-28 00:03:30 +0900

    바벨 config셋팅

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          chrome: "79", // 크롬 79까지 지원하는 코드를 만든다
+          ie: "11",
+        },
+        useBuiltIns: "usage", //'entry', false
+        corejs: {
+          version: 2, //3
+        },
+      },
+    ],
+  ],
+};
